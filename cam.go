0262ee48f298f64a808881074f9be3d8daa0ed3f
shogg <lars.heyden@gmail.com>
2012-03-31 22:00:38 +0200

    Implemented camera rotation with quaternions

--- cam.go
+++ cam.go
@@ -1,9 +1,5 @@
 package glvox
 
-import (
-	"math"
-)
-
 type Cam struct {
 	Pos, Dir, Up, Left Vec3
 }
@@ -34,75 +30,24 @@ func (c *Cam) Updown(a float32) {
 
 // Rotate up/down
 func (c *Cam) Pitch(a float32) {
-
-	sinA := float32(math.Sin(float64(a)))
-	cosA := float32(math.Cos(float64(a)))
-
-	c.Dir = Vec3 {
-		c.Dir.X,
-		cosA*c.Dir.Y - sinA*c.Dir.Z,
-		sinA*c.Dir.Y + cosA*c.Dir.Z,
-	}
-
-	c.Up = Vec3 {
-		c.Up.X,
-		cosA*c.Up.Y - sinA*c.Up.Z,
-		sinA*c.Up.Y + cosA*c.Up.Z,
-	}
-
-	c.Left = Vec3 {
-		c.Left.X,
-		cosA*c.Left.Y - sinA*c.Left.Z,
-		sinA*c.Left.Y + cosA*c.Left.Z,
-	}
+	q := NewQuat(a, c.Left)
+	m := q.Mat3()
+	c.Dir = m.Mul(c.Dir)
+	c.Up = m.Mul(c.Up)
 }
 
 // Rotate left/right
 func (c *Cam) Yaw(a float32) {
-
-	sinA := float32(math.Sin(float64(a)))
-	cosA := float32(math.Cos(float64(a)))
-
-	c.Dir = Vec3 {
-		sinA*c.Dir.Z + cosA*c.Dir.X,
-		c.Dir.Y,
-		cosA*c.Dir.Z - sinA*c.Dir.X,
-	}
-
-	c.Up = Vec3 {
-		sinA*c.Up.Z + cosA*c.Up.X,
-		c.Up.Y,
-		cosA*c.Up.Z - sinA*c.Up.X,
-	}
-
-	c.Left = Vec3 {
-		sinA*c.Left.Z + cosA*c.Left.X,
-		c.Left.Y,
-		cosA*c.Left.Z - sinA*c.Left.X,
-	}
+	q := NewQuat(a, c.Up)
+	m := q.Mat3()
+	c.Dir = m.Mul(c.Dir)
+	c.Left = m.Mul(c.Left)
 }
 
 // Rotate z
 func (c *Cam) Roll(a float32) {
-
-	sinA := float32(math.Sin(float64(a)))
-	cosA := float32(math.Cos(float64(a)))
-
-	c.Dir = Vec3 {
-		cosA*c.Dir.X - sinA*c.Dir.Y,
-		sinA*c.Dir.X + cosA*c.Dir.Y,
-		c.Dir.Z,
-	}
-
-	c.Up = Vec3 {
-		cosA*c.Up.X - sinA*c.Up.Y,
-		sinA*c.Up.X + cosA*c.Up.Y,
-		c.Up.Z,
-	}
-
-	c.Left = Vec3 {
-		cosA*c.Left.X - sinA*c.Left.Y,
-		sinA*c.Left.X + cosA*c.Left.Y,
-		c.Left.Z,
-	}
+	q := NewQuat(a, c.Dir)
+	m := q.Mat3()
+	c.Left = m.Mul(c.Left)
+	c.Up = m.Mul(c.Up)
 }
--- vec3.go
+++ vec3.go
@@ -8,6 +8,13 @@ type Vec3 struct {
 	X, Y, Z float32
 }
 
+type Mat3 [9]float32
+
+type Quat struct {
+	R float32
+	Vec3
+}
+
 func (a Vec3) Cross(b Vec3) Vec3 {
 	return Vec3 {
 			a.Y*b.Z - a.Z*b.Y,
@@ -38,4 +45,48 @@ func (a Vec3) Norm() float32 {
 
 func (a Vec3) Normalize() Vec3 {
 	return a.Mul(1.0/a.Norm())
+}
+
+func NewQuat(r float32, v Vec3) (q Quat) {
+
+	cosR := float32(math.Cos(float64(r/2)))
+	sinR := float32(math.Sin(float64(r/2)))
+
+	q = Quat { cosR, v.Mul(sinR) }
+	return q
+}
+
+func (q Quat) Mat3() (m Mat3) {
+
+	n := q.R*q.R + q.X*q.X + q.Y*q.Y + q.Z*q.Z
+	s := float32(0.0); if n > 0.0 { s = 2.0 / n }
+
+	xs, ys, zs := q.X*s,  q.Y*s,  q.Z*s
+
+	wx, wy, wz := q.R*xs, q.R*ys, q.R*zs
+	xx, xy, xz := q.X*xs, q.X*ys, q.X*zs
+	yy, yz, zz := q.Y*ys, q.Y*zs, q.Z*zs
+
+	m[0], m[1], m[2] = 1.0 - yy - zz,	xy - wz,		xz + wy
+	m[3], m[4], m[5] = xy + wz,			1.0 - xx - zz,	yz - wx
+	m[6], m[7], m[8] = xz - wy,			yz + wx,		1.0 - xx - yy
+
+	return m
+}
+
+func (q Quat) Mul(r Quat) (s Quat) {
+	s.R = q.R*r.R - q.X*r.X - q.Y*r.Y - q.Z*r.Z;
+	s.X = q.R*r.X + r.R*q.X + q.Y*r.Z - q.Z*r.Y;
+	s.Y = q.R*r.Y + r.R*q.Y + q.Z*r.X - q.X*r.Z;
+	s.Z = q.R*r.Z + r.R*q.Z + q.X*r.Y - q.Y*r.X;
+	return s;
+}
+
+func (m Mat3) Mul(v Vec3) (w Vec3) {
+	w = Vec3 {
+		m[0]*v.X + m[1]*v.Y + m[2]*v.Z,
+		m[3]*v.X + m[4]*v.Y + m[5]*v.Z,
+		m[6]*v.X + m[7]*v.Y + m[8]*v.Z,
+	}
+	return w
 }
